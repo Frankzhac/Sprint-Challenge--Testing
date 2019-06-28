91c2e53c00b2ee4706d752c0476c9d57c50329ae
Frankzhac <Frank.hemle@gmail.com>
2019-06-28 13:43:17 -0400

    I did things backwards and I am now having to write the tests for my server and index

--- index.test.js
+++ index.test.js
@@ -1,1 +1,12 @@
+const supertest = require('supertest');
+const server = require('./api/server.js');
 
+describe('listener', () => {
+  describe('listen', () => {
+    it('console.log the text "Listening on Port: 5000"', () => {
+      console.log = jest.fn();
+      console.log('Listening on Port: 5000');
+      expect(console.log.mock.calls[0][0]).toBe('Listening on Port: 5000')
+    });
+  });
+});
